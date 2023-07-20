7b3c860220fb7437e2fb6b66f81f72933169bd64
DanonePlayer <ce6519@gmail.com>
2023-07-20 15:51:43 -0500

    Update Estagio.py

--- Estagio.py
+++ Estagio.py
@@ -1,4 +1,4 @@
-#USA O javier-bardem3.jpg.png COMO EXEMPLO DE TESTE, ELE É MAIS COMO UMA BASE, É PARECIDO COM MUITAS DAS IMAGENS DA BASE QUE VAMOS USAR
+#USA O rosto.png COMO EXEMPLO DE TESTE, ELE É MAIS COMO UMA BASE, É PARECIDO COM MUITAS DAS IMAGENS DA BASE QUE VAMOS USAR
 import os
 from os.path import join as pjoin
 
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 from PIL import Image
 
-classificador = cv2.CascadeClassifier(r"anexos/eye.xml")
+classificador = cv2.CascadeClassifier(r"anexos/haarcascade_eye_tree_eyeglasses.xml")
 img = cv2.imread("hascas/rosto.png")
 
 
@@ -19,21 +19,22 @@ for x,y,l,a in objetos:
     # print(objetos)
     cv2.rectangle(img,(x,y),(x+l,y+a),(255, 0, 0), 2)
 
-olho_esquerdo = objetos[0]
-olho_direito = objetos[1]
+# olho_esquerdo = objetos[0]
+# olho_direito = objetos[1]
 
-img = cv2.imread("hascas/rosto.png")
-pts = np.array( [[olho_direito[0], olho_direito[1]],  [olho_direito[0], olho_direito[1]+olho_direito[3]], [olho_esquerdo[0]+[olho_esquerdo[2]], olho_esquerdo[1]+olho_esquerdo[3]], [olho_esquerdo[0]+[olho_esquerdo[2]], olho_esquerdo[1]]], np.int32)
+# img = cv2.imread("hascas/rosto.png")
+# pts = np.array( [[olho_direito[0], olho_direito[1]],  [olho_direito[0], olho_direito[1]+olho_direito[3]], [olho_esquerdo[0]+[olho_esquerdo[2]], olho_esquerdo[1]+olho_esquerdo[3]], [olho_esquerdo[0]+[olho_esquerdo[2]], olho_esquerdo[1]]], np.int32)
 
-# Cria uma máscara com os pontos
-mask = np.zeros_like(img)
-cv2.fillPoly(mask, [pts], (255, 255, 255))
+# # Cria uma máscara com os pontos
+# mask = np.zeros_like(img)
+# cv2.fillPoly(mask, [pts], (255, 255, 255))
 
 
-# Aplica a máscara na imagem original
-img_cortada = cv2.bitwise_and(img, mask)
-part_cortada = cv2.bitwise_and(img, cv2.bitwise_not(mask))
+# # Aplica a máscara na imagem original
+# img_cortada = cv2.bitwise_and(img, mask)
+# part_cortada = cv2.bitwise_and(img, cv2.bitwise_not(mask))
 
-cv2.imshow('Imagem cortada', part_cortada) #img #img_cortada #part_cortada
+print(objetos)
+cv2.imshow('Imagem cortada', img) #img #img_cortada #part_cortada
 cv2.waitKey(0)
 cv2.destroyAllWindows()
