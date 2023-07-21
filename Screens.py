6ce148a3f8fd925136ccd07920baff37d7822135
luizduarte12 <luiz.duarte@sou.ufac.br>
2023-07-20 20:44:17 -0500

    Tela Principal

--- Screens.py
+++ Screens.py
@@ -16,55 +16,61 @@ class Interface:
 
         ##Funções 01
 
-        btn = tk.Button(self.frm_left,width=15,height=1, bg='#D9D9D9',borderwidth=0)
-        btn.grid(row=0, column=0, padx=30, pady=(30, 5))
+        self.lbl_01 = tk.Label(self.frm_left,text="MENU",font=('Lato',16,'bold'), fg='#000', bg='#474444')
+        self.lbl_01.grid(row=0, column=0, padx=30, pady=(30, 5))
 
-        btn_01 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_01.grid(row=1, column=0, padx=30, pady=5)
+        btn = tk.Button(self.frm_left,width=15,height=1, bg='#D9D9D9',borderwidth=0, text='Word', font=('Arial',10,'bold'), fg='#474444')
+        btn.grid(row=1, column=0, padx=30, pady=(30, 5))
+
+        btn_01 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Corte', font=('Arial',10,'bold'), fg='#474444')
+        btn_01.grid(row=2, column=0, padx=30, pady=5)
 
         ##Funções02
 
-        btn_02 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_02.grid(row=2, column=0, padx=30, pady=(30, 5))
+        btn_02 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Especifico', font=('Arial',10,'bold'), fg='#474444')
+        btn_02.grid(row=3, column=0, padx=30, pady=(30, 5))
 
-        btn_03 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_03.grid(row=3, column=0, padx=30, pady=5)
+        btn_03 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Todo Branco', font=('Arial',10,'bold'), fg='#474444')
+        btn_03.grid(row=4, column=0, padx=30, pady=5)
 
         ##Funções03
 
-        btn_04 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_04.grid(row=4, column=0, padx=30, pady=(30, 5))
+        btn_04 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Rosto', font=('Arial',10,'bold'), fg='#474444')
+        btn_04.grid(row=5, column=0, padx=30, pady=(30, 5))
 
-        btn_05 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_05.grid(row=5, column=0, padx=30, pady=5)
+        btn_05 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Olhos', font=('Arial',10,'bold'), fg='#474444')
+        btn_05.grid(row=6, column=0, padx=30, pady=5)
 
-        btn_06 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_06.grid(row=6, column=0, padx=30, pady=5)
+        btn_06 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Nariz', font=('Arial',10,'bold'), fg='#474444')
+        btn_06.grid(row=7, column=0, padx=30, pady=5)
 
-        btn_07 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_07.grid(row=7, column=0, padx=30, pady=5)
+        btn_07 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0, text='Boca', font=('Arial',10, 'bold'), fg='#474444')
+        btn_07.grid(row=8, column=0, padx=30, pady=5)
 
         #Funcções_04
 
-        btn_08 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_08.grid(row=8, column=0, padx=30, pady=(30, 5))
+        btn_08 = tk.Button(self.frm_left, width=15, height=1, bg='#969696',borderwidth=0, text='Masculino', font=('Arial',10, 'bold'), fg='#fff')
+        btn_08.grid(row=9, column=0, padx=30, pady=(30, 5))
 
-        btn_09 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9',borderwidth=0)
-        btn_09.grid(row=9, column=0, padx=30, pady=5)
+        btn_09 = tk.Button(self.frm_left, width=15, height=1, bg='#969696',borderwidth=0, text='Feminino', font=('Arial',10, 'bold'), fg='#fff')
+        btn_09.grid(row=10, column=0, padx=30, pady=5)
 
         #Funções_05
 
-        btn_10 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9', borderwidth=0)
-        btn_10.grid(row=10, column=0, padx=30, pady=(30, 5))
+        btn_10 = tk.Button(self.frm_left, width=15, height=1, bg='#5B5A5A', borderwidth=0, text='Exportar', font=('Arial',10, 'bold'), fg='#fff')
+        btn_10.grid(row=11, column=0, padx=30, pady=(30, 5))
 
-        btn_11 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9', borderwidth=0)
-        btn_11.grid(row=11, column=0, padx=30, pady=5)
+        btn_11 = tk.Button(self.frm_left, width=15, height=1, bg='#5B5A5A', borderwidth=0, text='Salvar', font=('Arial',10, 'bold'), fg='#fff')
+        btn_11.grid(row=12, column=0, padx=30, pady=5)
 
-        btn_12 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9', borderwidth=0)
-        btn_12.grid(row=12, column=0, padx=30, pady=5)
+        btn_12 = tk.Button(self.frm_left, width=15, height=1, bg='#5B5A5A', borderwidth=0, text='Carregar', font=('Arial',10, 'bold'), fg='#fff')
+        btn_12.grid(row=13, column=0, padx=30, pady=5)
 
-        btn_13 = tk.Button(self.frm_left, width=15, height=1, bg='#D9D9D9', borderwidth=0)
-        btn_13.grid(row=13, column=0, padx=30, pady=5)
+        btn_13 = tk.Button(self.frm_left, width=15, height=1, bg='#5B5A5A', borderwidth=0, text='Novo', font=('Arial',10, 'bold'), fg='#fff')
+        btn_13.grid(row=14, column=0, padx=30, pady=5)
+
+        btn_14 = tk.Button(self.frm_left, width=15, height=1, bg='#5B5A5A', borderwidth=0, text='Sair',font=('Arial', 10, 'bold'), fg='#fff')
+        btn_14.grid(row=15, column=0, padx=30, pady=5)
 
 
 
